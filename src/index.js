a83717393dd417d05bb501a5b70e463fc8a22120
Michelle Mabuyo <michelle@apollographql.com>
2023-04-19 17:32:07 -0400

    fix: enable introspection

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ async function main() {
   );
   const server = new ApolloServer({
     schema: buildSubgraphSchema({ typeDefs, resolvers }),
+    introspection: true,
   });
   const { url } = await startStandaloneServer(server, {
     context: async ({ req }) => {
