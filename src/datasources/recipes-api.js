40228db7ad2940ae623947c4733f25affaca798e
Michelle Mabuyo <michelle@apollographql.com>
2023-05-05 11:59:12 -0400

    docs: add comments to DS methods

--- src/datasources/recipes-api.js
+++ src/datasources/recipes-api.js
@@ -3,15 +3,18 @@ const ingredientsData = require("./ingredients.json"); // from Airtable, exporte
 const utils = require("./recipe-utils");
 
 class RecipesAPI {
+  // Returns a random Recipe object from the full list of recipes
   getRandomRecipe() {
     const randomEntry = Math.floor(Math.random() * recipesData.length);
     return utils.formattedRecipe(recipesData[randomEntry]);
   }
 
+  // Returns all of the Recipe objects from the full list of recipes
   getAllRecipes() {
     return recipesData.map((r) => utils.formattedRecipe(r));
   }
 
+  // Returns a Recipe object based on an id (Airtable-generated ID)
   getRecipe(recipeId) {
     const recipe = recipesData.find((r) => r.id === recipeId);
     if (!recipe) {
@@ -20,6 +23,7 @@ class RecipesAPI {
     return utils.formattedRecipe(recipe);
   }
 
+  // Returns a list of Recipe objects (length decided by limit) that were most recently added
   // author's note: we're making this slow/unoptimized intentionally, for tutorial purposes :)
   async getRecentlyAddedRecipes(limit = 3) {
     const copiedData = [...recipesData];
@@ -33,6 +37,7 @@ class RecipesAPI {
     return mostRecent.map((r) => utils.formattedRecipe(r));
   }
 
+  // Returns a list of Ingredient objects based on the recipe ID (Airtable-generated)
   getRecipeIngredients(recipeId) {
     const recipe = recipesData.find((r) => r.id === recipeId);
 
@@ -46,6 +51,8 @@ class RecipesAPI {
         const ingredientName =
           ingredientsData.find((i) => i.id == ingredientId)?.fields?.name ||
           null;
+
+        // need to match the field names in the schema
         return {
           id: ingredientId,
           text: ingredient,
@@ -56,8 +63,9 @@ class RecipesAPI {
     return ingredientsForRecipe;
   }
 
+  // Returns a list of Recipe objects that are related to the given recipeId
   // author's note: we're making this slow/unoptimized intentionally, for tutorial purposes :)
-  async getRelatedRecipes(recipeId, limit) {
+  async getRelatedRecipes(recipeId, limit = 3) {
     const recipe = recipesData.find((r) => r.id === recipeId);
     if (!recipe) {
       throw new Error("Could not find recipe");
